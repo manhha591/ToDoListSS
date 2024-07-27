777abf47e9c562b27c9d6ffe1fd90d54bf7e6b41
manhha591 <64207033+manhha591@users.noreply.github.com>
2024-07-27 13:03:39 +0700

    calendar

---
+++ app/src/main/java/com/example/todolistss/ui/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.example.todolistss.ui;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.todolistss.R;
+
+import java.util.Calendar;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+            super.onBackPressed();
+
+            setResult(RESULT_OK);
+    }
+}
